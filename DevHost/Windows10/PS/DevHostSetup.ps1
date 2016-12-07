2ddc7e08af87117efa1f79109781e207e02191f7
wickedviking <wickedvikingstudios@gmail.com>
2016-12-07 13:02:34 -0600

    first commit

---
+++ DevHost/Windows10/PS/DevHostSetup.ps1
@@ -0,0 +1,13 @@
+﻿####################
+# Install Chocolatey
+####################
+Write-Host "Installing Chocolatey"
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+Write-Host
+
+#############################
+# Install Apps via Chocolatey
+#############################
+Write-Host "Installing Packages via Chocolatey"
+cinst git -y --params '"/GitAndUnixToolsOnPath"'
+Write-Host
---
+++ DevHost/Windows10/PS/TearDown.ps1
@@ -0,0 +1,8 @@
+﻿###############################################
+# undoes DevHostSetup.ps1, used for dev/testing
+###############################################
+
+###############################
+# Uninstall Apps via chocolatey
+###############################
+choco uninstall -yx git
