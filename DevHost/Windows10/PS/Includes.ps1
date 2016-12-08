50c8eb192e7591b7fc2dcf0205b1ff09a2764a85
wickedviking <wickedvikingstudios@gmail.com>
2016-12-08 13:55:51 -0600

    Get-From-Web.ps1 Updated Includes.ps1

--- DevHost/Windows10/PS/Includes.ps1
+++ DevHost/Windows10/PS/Includes.ps1
@@ -3,7 +3,11 @@ $output = Join-Path $PSScriptRoot "Add-Path.ps1"
 Start-BitsTransfer -Source $url -Destination $output
 .$output
 
-$url = "http://bit.ly/2hpM7bG"
-$output = Join-Path $PSScriptRoot "Add-Path.ps1"
+$url = "http://bit.ly/2hbiawN"
+$output = Join-Path $PSScriptRoot "Get-From-Web.ps1"
 Start-BitsTransfer -Source $url -Destination $output
+.$output
+
+$output = Join-Path $PSScriptRoot "Get-ComputerVirtualStatus.ps1"
+Get-From-Web "http://bit.ly/2hpK334" $output $false
 .$output
