4c84243cbe3a5d9ded5980681c63d2ebd1f92b22
wickedviking <wickedvikingstudios@gmail.com>
2016-12-08 14:12:52 -0600

    Get-From-Web.ps1

---
+++ DevHost/Windows10/PS/test/test.ps1
@@ -0,0 +1,13 @@
+﻿# Include Helper other helper functions
+$url = "http://bit.ly/2gGrLec"
+$output = Join-Path $PSScriptRoot "Includes.ps1"
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+.$output
+
+#$Credential = Get-Credential
+If(Get-ComputerVirtualStatus '10.101.120.21') {
+    Write-Host "True"
+} Else {
+    Write-Host "False"
+}
